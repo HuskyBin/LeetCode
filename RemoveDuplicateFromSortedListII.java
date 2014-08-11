d90639362c544d72e1d5c382e9f228325dbf600c
HuskyBin <xiang.zh@husky.neu.edu>
2014-08-11 00:47:09 -0400

    Create RemoveDuplicateFromSortedListII.java

---
+++ RemoveDuplicateFromSortedListII.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+    
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+        ListNode preNode = dummyNode;
+        ListNode pNode = head;
+        
+        while (pNode != null) {
+                if (pNode.next != null && (pNode.val == pNode.next.val)) {
+                while (pNode.next != null && pNode.val == pNode.next.val) {
+                    pNode = pNode.next;
+                }
+                pNode = pNode.next;
+                preNode.next = pNode;
+            }
+            else {
+                preNode = pNode;
+                pNode = pNode.next;
+            }
+        }
+    
+        head = dummyNode.next;
+        dummyNode.next = null;
+        return head;
+    }
+}
