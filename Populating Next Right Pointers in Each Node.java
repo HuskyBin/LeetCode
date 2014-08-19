12cd8cece418e0405162a48beea71bddc4915857
HuskyBin <xiang.zh@husky.neu.edu>
2014-08-19 16:17:04 -0400

    Update Populating Next Right Pointers in Each Node.java

--- Populating Next Right Pointers in Each Node.java
+++ Populating Next Right Pointers in Each Node.java
@@ -52,3 +52,32 @@ public class Solution {
         connectCore(pNode.right, pNode);
     }
 }
+
+
+// Very Neat Solution:
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        if (root == null) {
+            return;
+        }
+
+        TreeLinkNode parent = root;
+        TreeLinkNode next = parent.left;
+        while (parent != null && next != null) {
+            TreeLinkNode prev = null;
+            while (parent != null) {
+                if (prev == null) {
+                    prev = parent.left;
+                } else {
+                    prev.next = parent.left;
+                    prev = prev.next;
+                }
+                prev.next = parent.right;
+                prev = prev.next;
+                parent = parent.next;
+            }
+            parent = next;
+            next = parent.left;
+        }
+    }
+}
