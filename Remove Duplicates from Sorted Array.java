a27ca5c1cf5f4075506e14bcf2218816ba53828b
HuskyBin <xiang.zh@husky.neu.edu>
2014-08-30 16:48:42 -0400

    Create Remove Duplicates from Sorted Array.java

---
+++ Remove Duplicates from Sorted Array.java
@@ -0,0 +1,25 @@
+/*
+Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+
+Do not allocate extra space for another array, you must do this in place with constant memory.
+
+For example,
+Given input array A = [1,1,2],
+
+Your function should return length = 2, and A is now [1,2].
+*/
+public class Solution {
+    public int removeDuplicates(int[] A) {
+        if (A == null || A.length == 0) {
+            return 0;
+        }
+        int newIndex = 1;
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] != A[i - 1]) {
+                A[newIndex] = A[i];
+                newIndex++;
+            }
+        }
+        return newIndex;
+    }
+}
