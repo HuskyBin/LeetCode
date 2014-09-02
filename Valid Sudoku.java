47e4a929c85dd5108b81084d8ad7f9dbc9e2da18
HuskyBin <xiang.zh@husky.neu.edu>
2014-09-02 16:14:32 -0400

    Create Valid Sudoku.java

---
+++ Valid Sudoku.java
@@ -0,0 +1,60 @@
+/*
+Determine if a Sudoku is valid, according to: Sudoku Puzzles - The Rules.
+
+The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+
+
+A partially filled sudoku which is valid.
+
+Note:
+A valid Sudoku board (partially filled) is not necessarily solvable. Only the filled cells need to be validated.
+
+
+*/
+public class Solution {
+    
+    public boolean isValidSudoku(char[][] board) {
+        if (board == null || board.length == 0 || board[0].length == 0) {
+            return false;
+        }
+        int[][] cell = new int[3][3];
+        int[] row = new int[board.length];
+        int[] column = new int[board[0].length];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == '.') {
+                    continue;
+                }
+                if (updateRowOrColumn(row, i, board[i][j]) == false) {
+                    return false;
+                }
+                if (updateRowOrColumn(column, j, board[i][j]) == false) {
+                    return false;
+                }
+                int cellRow = i / 3;
+                int cellColumn = j / 3;
+                if (updateCell(cell, cellRow, cellColumn, board[i][j]) == false) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    
+    private boolean updateRowOrColumn(int[] number, int index, int cellNumber) {
+        if ((number[index] & (1 << cellNumber)) != 0) {
+            return false;
+        }
+        number[index] |= (1 << cellNumber);
+        return true;
+    }
+    
+    private boolean updateCell(int[][] cell, int cellRow, int cellColumn, int cellNumber) {
+        if ((cell[cellRow][cellColumn] & (1 << cellNumber)) != 0) {
+            return false;
+        }
+        cell[cellRow][cellColumn] |= (1 << cellNumber);
+        return true;
+    }
+}
