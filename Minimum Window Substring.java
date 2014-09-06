1bca9055e38163b1ab910abe330e590c932390a2
HuskyBin <xiang.zh@husky.neu.edu>
2014-09-06 15:41:18 -0400

    Update Minimum Window Substring.java

--- Minimum Window Substring.java
+++ Minimum Window Substring.java
@@ -13,6 +13,9 @@ If there are multiple such windows, you are guaranteed that there will always be
 
 
 */
+
+//双指针，动态维护一个区间。尾指针不断往后扫，当扫到有一个窗口包含了所有T的字符后，然后再收缩头指针，直到不能再收缩为止。最后记录所有可能的情况中窗口最小的
+
 public class Solution {
     public String minWindow(String S, String T) {
         if (S == null || S == null) {
