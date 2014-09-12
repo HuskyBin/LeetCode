c3ac6ecc8ef4ee2c234b3c7f7f470536b845843f
HuskyBin <xiang.zh@husky.neu.edu>
2014-09-12 15:59:27 -0400

    Update and rename CopyListwithRandomPointer.java to Copy List with Random Pointer.java

---
+++ Copy List with Random Pointer.java
@@ -0,0 +1,83 @@
+/*
+A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
+
+Return a deep copy of the list.
+*/
+public class Solution {
+    public RandomListNode copyRandomList(RandomListNode head) {
+        if (head == null) {
+            return head;
+        }
+        Map<RandomListNode, RandomListNode> cloneNodeMap = new HashMap<>();
+        RandomListNode pNode = head;
+        while (pNode != null) {
+            RandomListNode newCloneNode = new RandomListNode(pNode.label);
+            cloneNodeMap.put(pNode, newCloneNode);
+            pNode = pNode.next;
+        }
+        pNode = head;
+        while (pNode != null) {
+            RandomListNode cloneNode = cloneNodeMap.get(pNode);
+            RandomListNode nextNode = pNode.next;
+            if (nextNode == null) {
+                cloneNode.next = null;
+            }
+            else {
+                cloneNode.next = cloneNodeMap.get(nextNode);
+            }
+            RandomListNode randomNode = pNode.random;
+            if (randomNode == null) {
+                cloneNode.random = null;
+            }
+            else {
+                cloneNode.random = cloneNodeMap.get(randomNode);
+            }
+            pNode = nextNode;
+        }
+        return cloneNodeMap.get(head);
+    }
+}
+
+// version Two
+public class Solution {
+    public RandomListNode copyRandomList(RandomListNode head) {
+        if (head == null) {
+            return head;
+        }
+        RandomListNode pNode = head;
+        while (pNode != null) {
+            RandomListNode newNode = new RandomListNode(pNode.label);
+            RandomListNode nextNode = pNode.next;
+            newNode.next = nextNode;
+            pNode.next = newNode;
+            pNode = nextNode;
+        }
+        pNode = head;
+        while (pNode != null) {
+            RandomListNode nextRandomNode = pNode.next;
+            if (pNode.random != null) {
+                nextRandomNode.random = pNode.random.next;
+            }
+            pNode = nextRandomNode.next;
+        }
+        
+        RandomListNode newDummyNode = new RandomListNode(-1);
+        RandomListNode oldDummyNode = new RandomListNode(-1);
+        
+        RandomListNode newLink = newDummyNode;
+        RandomListNode newOld = oldDummyNode;
+        pNode = head;
+        while (pNode != null) {
+            newOld.next = pNode;
+            newLink.next = pNode.next;
+            newOld = newOld.next;
+            newLink = newLink.next;
+            pNode = pNode.next.next;
+        }
+        newOld.next = null;
+        oldDummyNode.next = null;
+        RandomListNode copyHead = newDummyNode.next;
+        newDummyNode.next = null;
+        return copyHead;
+    }
+}
