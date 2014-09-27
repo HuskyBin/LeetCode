28fbcbdd2e0c2abddeddc2509e7fd8f12bee8974
HuskyBin <xiang.zh@husky.neu.edu>
2014-09-27 15:42:47 -0400

    Update Interleaving String.java

--- Interleaving String.java
+++ Interleaving String.java
@@ -46,3 +46,41 @@ public class Solution {
         return false;
     }
 }
+
+
+
+// For Loop DP
+public boolean isInterleave(String s1, String s2, String s3) {
+    if (s1 == null || s2 == null || s3 == null || (s1.length() + s2.length()) != s3.length()) {
+        return false;
+    }
+    
+    boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
+    dp[0][0] = true;
+    for (int i = 1; i < s1.length() + 1; i++) {
+        if (s1.charAt(i - 1) != s3.charAt(i - 1)) {
+            break;
+        }
+        dp[i][0] = true;
+    }
+    
+    for (int i = 1; i < s2.length() + 1; i++) {
+        if (s2.charAt(i - 1) != s3.charAt(i - 1)) {
+            break;
+        }
+        dp[0][i] = true;
+    }
+    
+    for (int i = 1; i < s1.length() + 1; i++) {
+        for (int j = 1; j < s2.length() + 1; j++) {
+            if (s1.charAt(i - 1) == s3.charAt(i + j - 1) && dp[i - 1][j]) {
+                dp[i][j] = true;
+            }
+            else if (s2.charAt(j - 1) == s3.charAt(i + j - 1) && dp[i][j - 1]) {
+                dp[i][j] = true;
+            }
+        }
+    }
+    
+    return dp[s1.length()][s2.length()];
+}
