a9b979f3dd928287e2e3ed54c816097b4eca63e9
HuskyBin <xiang.zh@husky.neu.edu>
2014-11-07 16:42:32 -0500

    Update Unique Paths II.java

--- Unique Paths II.java
+++ Unique Paths II.java
@@ -44,3 +44,42 @@ public class Solution {
     }
     
 }
+
+
+
+// Solution 2
+public class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid == null || obstacleGrid.length == 0 || obstacleGrid[0].length == 0) {
+            return 0;
+        }
+        int row = obstacleGrid.length;
+        int column = obstacleGrid[0].length;
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[row - 1][column - 1] == 1) {
+            return 0;
+        }
+        int[][] dp = new int[row][column];
+        dp[0][0] = 1;
+        for (int i = 0; i < row; i++) {
+            if (obstacleGrid[i][0] == 1) {
+                break;
+            }
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i < column; i++) {
+            if (obstacleGrid[0][i] == 1) {
+                break;
+            }
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < column; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    continue;
+                }
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[row - 1][column - 1];
+    }
+}
