de96bf6e3ca0104cdd81e6e9427cc25d6c31cc8c
HuskyBin <xiang.zh@husky.neu.edu>
2014-12-31 15:48:02 -0500

    Create Excel Sheet Column Title.java

---
+++ Excel Sheet Column Title.java
@@ -0,0 +1,41 @@
+/*
+Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+
+For example:
+
+    1 -> A
+    2 -> B
+    3 -> C
+    ...
+    26 -> Z
+    27 -> AA
+    28 -> AB 
+Credits:
+Special thanks to @ifanchu for adding this problem and creating all test cases.
+*/
+public class Solution {
+    public String convertToTitle(int n) {
+        if (n < 0) {
+            return null;
+        }
+        StringBuilder sb = new StringBuilder();
+
+        while ( n > 0) {
+            int number = 26;
+            int remander = n % number;
+            if (remander == 0) {
+                sb.append('Z');
+            }
+            else {
+                sb.append((char)('A' + (remander - 1)));
+            }
+            if (remander == 0) {
+                n = n / 26 - 1;
+            }
+            else {
+                n /= 26;
+            }
+        }
+        return sb.reverse().toString();
+    }
+}
