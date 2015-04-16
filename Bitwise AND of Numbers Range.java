942e1bf21d2f89f4c58573a6261312c1967d08bf
HuskyBin <xiang.zh@husky.neu.edu>
2015-04-16 15:00:33 -0400

    Update Bitwise AND of Numbers Range.java

--- Bitwise AND of Numbers Range.java
+++ Bitwise AND of Numbers Range.java
@@ -6,16 +6,17 @@ For example, given the range [5, 7], you should return 4.
 Credits:
 Special thanks to @amrsaqr for adding this problem and creating all test cases.
 */
-public class Solution {
+public class BitwiseANDofNumbersRange {
+    
     public int rangeBitwiseAnd(int m, int n) {
         int diff = n - m;
-    	int result = n;
-    	for (int i = 0; i < 32 && diff > 0; i++) {
-    		if ((result & (1 << i)) != 0) {
-    			diff -= (int)Math.pow(2.0, i);
-    			result &= ~(1 << i);
-    		}
-    	}
-    	return result;
+        int result = n;
+        for (int i = 0; i < 32 && diff > 0; i++) {
+            if ((result & (1 << i)) != 0) {
+                diff -= (int)Math.pow(2.0, i);
+                result &= ~(1 << i);
+            }
+        }
+        return result;
     }
 }
