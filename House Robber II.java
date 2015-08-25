024286343efac2aea52de366b989bc66e64b56ee
HuskyBin <xiang.zh@husky.neu.edu>
2015-08-25 19:34:00 -0400

    Create House Robber II.java

---
+++ House Robber II.java
@@ -0,0 +1,32 @@
+/*
+Note: This is an extension of House Robber.
+
+After robbing those houses on that street, the thief has found himself a new place for his thievery so that he will not get too much attention. This time, all houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, the security system for these houses remain the same as for those in the previous street.
+
+Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+
+Credits:
+Special thanks to @Freezen for adding this problem and creating all test cases.
+*/
+public class Solution {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int[] dp = new int[nums.length + 1];
+        dp[1] = nums[0];
+        for (int i = 1; i < nums.length - 1; i++) {
+            dp[i + 1] = Math.max(dp[i], dp[i - 1] + nums[i]);
+        }
+        int result = dp[nums.length - 1];
+        dp = new int[nums.length + 1];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i + 1] = Math.max(dp[i], dp[i - 1] + nums[i]);
+        }
+        result = Math.max(result, dp[nums.length]);
+        return result;
+    }
+}
