250cb393beda2351a74edefcd771b7f38897a9f8
HuskyBin <xiang.zh@husky.neu.edu>
2015-10-10 16:19:09 -0400

    Create Invert Binary Tree.java

---
+++ Invert Binary Tree.java
@@ -0,0 +1,29 @@
+/*
+Invert a binary tree.
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+to
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+*/
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
