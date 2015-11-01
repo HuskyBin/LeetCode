f9f9b91848f0ed4374023febce4eb47c05512dbd
HuskyBin <xiang.zh@husky.neu.edu>
2015-11-01 13:24:58 -0500

    Update Bulls and Cows.java

--- Bulls and Cows.java
+++ Bulls and Cows.java
@@ -45,3 +45,34 @@ public class Solution {
         return String.valueOf(hit) + "A" + String.valueOf(miss) + "B";
     }
 }
+
+
+
+// Best Solution
+  public String getHint(String secret, String guess) {
+         
+        if (secret == null || guess == null || secret.length() != guess.length()) {
+            return "";
+        }
+         
+        int countA = 0;
+        int countB = 0;
+        int[] count = new int[10];
+         
+        for (int i = 0; i < secret.length(); i++) {
+            if (secret.charAt(i) == guess.charAt(i)) {
+                countA++;
+            } else {
+                count[secret.charAt(i) - '0']++;
+                if (count[secret.charAt(i) - '0'] <= 0) {
+                    countB++;
+                }
+                count[guess.charAt(i)- '0']--;
+                if (count[guess.charAt(i)- '0'] >= 0) {
+                    countB++;
+                }
+            }
+        }
+         
+        return String.valueOf(countA) + "A" + String.valueOf(countB) + "B";
+    }
