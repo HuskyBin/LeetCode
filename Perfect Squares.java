3fbc24c56dd44b92c431e97c9f8f8d65682496a5
HuskyBin <xiang.zh@husky.neu.edu>
2016-04-28 13:06:52 -0400

    Update Perfect Squares.java

--- Perfect Squares.java
+++ Perfect Squares.java
@@ -33,3 +33,23 @@ public class Solution {
         return dp[n];
     }
 }
+
+// N * Sqrt(n) Solution
+public class Solution {
+    public int numSquares(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        int[] dp= new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        for (int i = 1; i * i <= n; i++) {
+            dp[i * i] = 1;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; i + j * j <= n; j++) {
+                dp[i + j * j] = Math.min(dp[i + j * j], dp[i] + 1);
+            }
+        }
+        return dp[n];
+    }
+}
