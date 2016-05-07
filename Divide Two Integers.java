9247d28b1cfdd43f5467bedd7cab848dfb800b96
HuskyBin <xiang.zh@husky.neu.edu>
2016-05-06 23:30:25 -0400

    Update Divide Two Integers.java

--- Divide Two Integers.java
+++ Divide Two Integers.java
@@ -2,7 +2,15 @@
 Divide two integers without using multiplication, division and mod operator.
 */
 public class Solution {
-    public int divide(int dividend, int divisor) {
+    /**
+     * @param dividend the dividend
+     * @param divisor the divisor
+     * @return the result
+     */
+    // public int divide(int dividend, int divisor) {
+    //     // Write your code here
+    // }
+     public int divide(int dividend, int divisor) {
         if (divisor == 0) {
             return Integer.MAX_VALUE;
         }
@@ -12,11 +20,14 @@ public class Solution {
         boolean signNegative = (dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0);
         long tempDividend = Math.abs((long) dividend);
         long tempDivisor = Math.abs((long) divisor);
-        int result = (int)divideCore(tempDividend, tempDivisor);
+        long result = divideCore(tempDividend, tempDivisor);
         if (signNegative == true) {
-            return -result;
+            result = -result;
         }
-        return result;
+        if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+        return (int)result;
     }
     
     private long divideCore(long dividend, long divisor) {
@@ -29,7 +40,7 @@ public class Solution {
         
         long closestPowNumber = findTheClosestPowNumber(dividend, divisor);
         long subResult = divideCore(dividend - (divisor << closestPowNumber), divisor);
-        return (1 << closestPowNumber) + subResult;
+        return ((long)1 << closestPowNumber) + subResult;
     }
     
     private long findTheClosestPowNumber(long dividend, long divisor) {
