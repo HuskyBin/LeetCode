7da5179ed67befb5914e04cb9ca805b7410876ad
HuskyBin <xiang.zh@husky.neu.edu>
2016-06-15 23:28:02 -0400

    Update Reverse Words in a String II.java

--- Reverse Words in a String II.java
+++ Reverse Words in a String II.java
@@ -47,3 +47,30 @@ public class Solution {
         }
     }
 }
+
+
+//Better Solution
+
+public class Solution {
+    public void reverseWords(char[] s) {
+        if (s.length == 0) return;
+        reverse(s, 0, s.length-1);
+        int last = 0;
+        for (int i=0; i<=s.length; i++) {
+            if (i == s.length || s[i] == ' ') {
+                reverse(s, last, i-1);
+                last = i + 1;
+            }
+        }
+    }
+    
+    public void reverse(char[] s, int l, int r) {
+        while (l <= r) {
+            char temp = s[l];
+            s[l] = s[r];
+            s[r] = temp;
+            l++;
+            r--;
+        }
+    }
+}
