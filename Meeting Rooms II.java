4335b5a122fcb7464b1933bf5bc9d4e98af8c88a
HuskyBin <xiang.zh@husky.neu.edu>
2016-09-15 21:56:19 -0400

    Create  Meeting Rooms II.java

---
+++ Meeting Rooms II.java
@@ -0,0 +1,45 @@
+/*
+Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms required.
+
+For example,
+Given [[0, 30],[5, 10],[15, 20]],
+return 2.
+*/
+public class Solution {
+    public int minMeetingRooms(Interval[] intervals) {
+        if (intervals == null || intervals.length == 0) {
+            return 0;
+        }
+        int[] startArr = new int[intervals.length];
+        int[] endArr = new int[intervals.length];
+        Arrays.sort(intervals, (a, b) -> (a.start - b.start));
+        for (int i = 0; i< intervals.length; i++) {
+            startArr[i] = intervals[i].start;
+        }
+        Arrays.sort(intervals, (a, b) -> (a.end - b.end));
+        for (int i = 0; i < intervals.length; i++) {
+            endArr[i] = intervals[i].end;
+        }
+        int result = 0;
+        int start = 0;
+        int end = 0;
+        int max = 0;
+        while (start < intervals.length && end < intervals.length) {
+            if (startArr[start] < endArr[end]) {
+                result++;
+                max = Math.max(max, result);
+                start++;
+            }
+            else if (startArr[start] > endArr[end]) {
+                result--;
+                end++;
+            }
+            else {
+                start++;
+                end++;
+            }
+        }
+        return max;
+        
+    }
+}
