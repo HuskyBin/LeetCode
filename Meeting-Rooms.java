d7a3b4d51dc9494521d4daae60f89039fde65423
HuskyBin <xiang.zh@husky.neu.edu>
2016-09-15 21:41:37 -0400

    Update Meeting-Rooms.java

--- Meeting-Rooms.java
+++ Meeting-Rooms.java
@@ -32,3 +32,19 @@ public class Solution {
         return true;
     }
 }
+
+// clean sort
+public class Solution {
+    public boolean canAttendMeetings(Interval[] intervals) {
+        if (intervals == null || intervals.length == 0) {
+            return true;
+        }
+        Arrays.sort(intervals, (a, b) -> (a.start - b.start));
+        for (int i = 0; i < intervals.length - 1; i++) {
+            if (intervals[i].end > intervals[i + 1].start) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
