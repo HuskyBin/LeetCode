212edba226ef077a6eb43f13cfb40d36032a3437
HuskyBin <xiang.zh@husky.neu.edu>
2016-09-15 23:03:03 -0400

    Create Factor-Combinations.java

---
+++ Factor-Combinations.java
@@ -0,0 +1,57 @@
+/*
+Numbers can be regarded as product of its factors. For example,
+
+8 = 2 x 2 x 2;
+  = 2 x 4.
+Write a function that takes an integer n and return all possible combinations of its factors.
+
+Note: 
+You may assume that n is always positive.
+Factors should be greater than 1 and less than n.
+Examples: 
+input: 1
+output: 
+[]
+input: 37
+output: 
+[]
+input: 12
+output:
+[
+  [2, 6],
+  [2, 2, 3],
+  [3, 4]
+]
+input: 32
+output:
+[
+  [2, 16],
+  [2, 2, 8],
+  [2, 2, 2, 4],
+  [2, 2, 2, 2, 2],
+  [2, 4, 4],
+  [4, 8]
+]
+*/
+public class Solution {
+    public List<List<Integer>> getFactors(int n) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> oneResult = new ArrayList<>();
+        getCore(n, result, oneResult, 0);
+        return result;
+    }
+    
+    private void getCore(int n, List<List<Integer>> result, List<Integer> oneResult, int last) {
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0 && i >= last) {
+                oneResult.add(i);
+                oneResult.add(n / i);
+                List<Integer> newList = new ArrayList<>(oneResult);
+                result.add(newList);
+                oneResult.remove(oneResult.size() - 1);
+                getCore(n / i, result, oneResult, i);
+                oneResult.remove(oneResult.size() - 1);
+            }
+        }
+    }
+}
