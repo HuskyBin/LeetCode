b7f931430457463bc7a01d8e71eb361820f89163
HuskyBin <xiang.zh@husky.neu.edu>
2016-10-06 19:01:02 -0400

    Create Paint House II.java

---
+++ Paint House II.java
@@ -0,0 +1,45 @@
+/*
+There are a row of n houses, each house can be painted with one of the k colors. The cost of painting each house with a certain color is different. You have to paint all the houses such that no two adjacent houses have the same color.
+
+The cost of painting each house with a certain color is represented by a n x k cost matrix. For example, costs[0][0] is the cost of painting house 0 with color 0; costs[1][2] is the cost of painting house 1 with color 2, and so on... Find the minimum cost to paint all houses.
+
+Note:
+All costs are positive integers.
+
+Follow up:
+Could you solve it in O(nk) runtime?
+*/
+public class Solution {
+    public int minCostII(int[][] costs) {
+        if (costs == null || costs.length == 0) {
+            return 0;
+        }
+        int[] color = new int[costs[0].length];
+        for (int i = 0; i < color.length; i++) {
+            color[i] = costs[0][i];
+        }
+        
+        for (int i = 1; i < costs.length; i++) {
+            int[] nextColor = new int[costs[0].length];
+            for (int j = 0; j < costs[0].length; j++) {
+                int min = Integer.MAX_VALUE;
+                int index = 0;
+                while (index < costs[0].length) {
+                    if (index != j) {
+                        min = Math.min(min, color[index]);
+                    }
+                    index++;
+                }
+                nextColor[j] = min + costs[i][j];
+            }
+            for (int k = 0; k < costs[0].length; k++) {
+                color[k] = nextColor[k];
+            }
+        }
+        int result = color[0];
+        for (int i = 1; i < color.length; i++) {
+            result = Math.min(result, color[i]);
+        }
+        return result;
+    }
+}
