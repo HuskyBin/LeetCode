f9c60cd9fc2a8e38d86a7757e6bd49deca852e02
HuskyBin <xiang.zh@husky.neu.edu>
2016-10-12 23:28:05 -0400

    Update Find the Difference.java

--- Find the Difference.java
+++ Find the Difference.java
@@ -17,6 +17,21 @@ e
 Explanation:
 'e' is the letter that was added.
 */
+
+// Bit
+
+public class Solution {
+    public char findTheDifference(String s, String t) {
+       char c = t.charAt(t.length() - 1);
+       for (int i = 0; i < s.length(); i++) {
+           c ^= s.charAt(i);
+           c ^= t.charAt(i);
+       }
+       return c;
+    }
+}
+
+// Map
 public class Solution {
     public char findTheDifference(String s, String t) {
         if (s == null || t == null) {
