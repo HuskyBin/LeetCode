0913fdfbedfa307d156fd93a77cbeb381182d73b
HuskyBin <xiang.zh@husky.neu.edu>
2016-10-12 23:11:28 -0400

    Update Largest BST Subtree.java

--- Largest BST Subtree.java
+++ Largest BST Subtree.java
@@ -24,6 +24,49 @@ Can you figure out ways to solve it with O(n) time complexity?
  *     TreeNode(int x) { val = x; }
  * }
  */
+
+
+//简介代码
+
+public class Solution {
+    public class SuperNode {
+        int ans;
+        int small, large;
+        boolean isBST;
+        public SuperNode() {
+            ans = 0;
+            isBST = true;
+            small = Integer.MAX_VALUE;
+            large = -Integer.MAX_VALUE;
+        }
+    }
+    public int largestBSTSubtree(TreeNode root) {
+        return dfs(root).ans;
+    }
+    public SuperNode dfs(TreeNode node) {
+        if (node == null) {
+            return new SuperNode();
+        }
+        SuperNode now = new SuperNode();
+        SuperNode left = dfs(node.left);
+        SuperNode right = dfs(node.right);
+        if (left.small < node.val) {
+            now.small = left.small;
+        } else {
+            now.small = node.val;
+        }
+        now.large = Math.max(right.large,node.val);
+        if (left.isBST && right.isBST && left.large <= node.val && right.small >= node.val) {
+            now.ans = left.ans + right.ans +1;
+            now.isBST = true;
+        } else {
+            now.ans=Math.max(left.ans,right.ans);
+            now.isBST = false;
+        }
+        return now;
+    }
+}
+//我的 啰嗦代码
 public class Solution {
     
     private class Result {
