55f2c6682e2f92d4266f64d37cd4628e9d15e587
HuskyBin <xiang.zh@husky.neu.edu>
2016-12-01 18:20:00 -0500

    Update Repeated Substring Pattern.java

--- Repeated Substring Pattern.java
+++ Repeated Substring Pattern.java
@@ -53,3 +53,19 @@ public class Solution {
         return true;
     }
 }
+
+//Python
+
+class Solution(object):
+    def repeatedSubstringPattern(self, str):
+        """
+        :type str: str
+        :rtype: bool
+        """
+        size = len(str)
+        for x in range(1, size / 2 + 1):
+            if (size % x != 0):
+                continue;
+            if (str[:x] * (size / x) == str):
+                return True
+        return False
