7aeeed8264de7998d4950fcdd2217a5e70746677
HuskyBin <xiang.zh@husky.neu.edu>
2017-01-25 22:13:38 -0500

    Update Number Complement.java

--- Number Complement.java
+++ Number Complement.java
@@ -31,3 +31,13 @@ public class Solution {
         return ~num & number;
     }
 }
+
+// Better Solution
+
+public class Solution {
+    public int findComplement(int num) {
+        int mask = ~0;
+        while ((num & mask) != 0) mask <<=  1;
+        return ~num & ~mask;
+    }
+}
