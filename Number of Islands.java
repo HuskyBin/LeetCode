6d0fc2ee3b6bbaeae8f8e37afa9aa6c03a474a51
HuskyBin <xiang.zh@husky.neu.edu>
2017-02-05 22:02:43 -0500

    Update Number of Islands.java

--- Number of Islands.java
+++ Number of Islands.java
@@ -56,3 +56,76 @@ public class Solution {
     	numIslandsCore(grid, visited, x, y - 1);
     }
 }
+
+
+// 02/05/2017  DFS
+public class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int sum = 0;
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (visited[i][j] == false && grid[i][j] == '1') {
+                    findIsland(grid, i, j, visited);
+                    sum ++;
+                }
+            }
+        }
+        return sum;
+    }
+    
+    private void findIsland(char[][] grid, int row, int column, boolean[][] visited) {
+        if (row < 0 || row >= grid.length || column < 0 || column >= grid[0].length) {
+            return;
+        }
+        if (grid[row][column] == '0') {
+            return;
+        }
+        if (visited[row][column] == true) {
+            return;
+        }
+        visited[row][column] = true;
+        int[][] dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        for (int i = 0; i < dir.length; i++) {
+            int newRow = row + dir[i][0];
+            int newColumn = column + dir[i][1];
+            findIsland(grid, newRow, newColumn, visited);
+        }
+    }
+}
+
+// BFS
+
+public class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int sum = 0;
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (visited[i][j] == false && grid[i][j] == '1') {
+                    findIsland(grid, i, j, visited);
+                    sum ++;
+                }
+            }
+        }
+        return sum;
+    }
+    
+    private void findIsland(char[][] grid, int row, int column, boolean[][] visited) {
+        Queue<Integer> rowQueue = new LinkedList<>();
+        Queue<Integer> columnQueue = new LinkedList<>();
+        rowQueue.add(row);
+        columnQueue.add(column);
+        visited[row][column] = true;
+        while (!rowQueue.isEmpty()) {
+            int topRow = rowQueue.poll();
+            int topColumn = columnQueue.poll();
+            int[][] dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+            for (int i = 0; i < dir.length; i++) {
+  
