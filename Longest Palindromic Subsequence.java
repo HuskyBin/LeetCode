9c86e5c30306fb5d14ca65a49dd7aa8d14ea0610
HuskyBin <xiang.zh@husky.neu.edu>
2017-03-01 23:29:26 -0500

    Create Longest Palindromic Subsequence.java

---
+++ Longest Palindromic Subsequence.java
@@ -0,0 +1,47 @@
+/*
+Given a string s, find the longest palindromic subsequence's length in s. You may assume that the maximum length of s is 1000.
+
+Example 1:
+Input:
+
+"bbbab"
+Output:
+4
+One possible longest palindromic subsequence is "bbbb".
+Example 2:
+Input:
+
+"cbbd"
+Output:
+2
+One possible longest palindromic subsequence is "bb".
+*/
+public class Solution {
+    public int longestPalindromeSubseq(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int[][] dp = new int[s.length()][s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            dp[i][i] = 1;
+        }
+        for (int len = 2; len <= s.length(); len++) {
+            for (int i = 0; i + len - 1 < s.length(); i++) {
+                int j = i + len - 1;
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                    if (len > 2) {
+                        dp[i][j] = Math.max(dp[i][j], dp[i + 1][j - 1] + 2);
+                    }
+                    else {
+                        dp[i][j] = Math.max(dp[i][j], 2);
+                    }
+                }
+                else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[0][s.length() - 1];
+    }
+}
