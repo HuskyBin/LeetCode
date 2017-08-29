1d699c6cc2006de341746e2b0a2d3daa31085351
HuskyBin <xiang.zh@husky.neu.edu>
2017-08-29 19:45:19 -0400

    Create Subtree of Another Tree.java

---
+++ Subtree of Another Tree.java
@@ -0,0 +1,71 @@
+/*
+Given two non-empty binary trees s and t, check whether tree t has exactly the same structure and node values with a subtree of s. A subtree of s is a tree consists of a node in s and all of this node's descendants. The tree s could also be considered as a subtree of itself.
+
+Example 1:
+Given tree s:
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+Given tree t:
+   4 
+  / \
+ 1   2
+Return true, because t has the same structure and node values with a subtree of s.
+Example 2:
+Given tree s:
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+    /
+   0
+Given tree t:
+   4
+  / \
+ 1   2
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        }
+        if (s == null || t == null) {
+            return false;
+        }
+        if (s.val == t.val) {
+            if (checkCore(s, t)) {
+                return true;
+            }
+        }
+        boolean left = isSubtree(s.left, t);
+        boolean result = left || isSubtree(s.right, t);
+        return result;
+    }
+    
+    private boolean checkCore(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        }
+        if (s == null || t == null) {
+            return false;
+        }
+        if (s.val != t.val) {
+            return false;
+        }
+        return checkCore(s.left, t.left) && checkCore(s.right, t.right);
+    }
+}
