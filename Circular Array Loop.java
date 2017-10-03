c81b3fed3f6803888bf57b7b6899809308da4c45
HuskyBin <xiang.zh@husky.neu.edu>
2017-10-02 22:14:39 -0400

    Create Circular Array Loop.java

---
+++ Circular Array Loop.java
@@ -0,0 +1,35 @@
+/*
+You are given an array of positive and negative integers. If a number n at an index is positive, then move forward n steps. Conversely, if it's negative (-n), move backward n steps. Assume the first element of the array is forward next to the last element, and the last element is backward next to the first element. Determine if there is a loop in this array. A loop starts and ends at a particular index with more than 1 element along the loop. The loop must be "forward" or "backward'.
+
+Example 1: Given the array [2, -1, 1, 2, 2], there is a loop, from index 0 -> 2 -> 3 -> 0.
+
+Example 2: Given the array [-1, 2], there is no loop.
+
+Note: The given array is guaranteed to contain no element "0".
+
+Can you do it in O(n) time complexity and O(1) space complexity?
+*/
+class Solution {
+    public boolean circularArrayLoop(int[] nums) {
+        if (nums == null) {
+            return false;
+        }   
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0 && dfs(nums, i, 0) != 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private int dfs(int[] nums, int index, int count) {
+        if (count >= nums.length) {
+            return nums[index];
+        }
+        int newIndex = (index + nums[index] + nums.length) % nums.length;
+        if (newIndex == index || nums[newIndex] * nums[index] < 0 || dfs(nums, newIndex, count + 1) == 0) {
+            nums[index] = 0;
+        }
+        return nums[index];
+    }
+}
