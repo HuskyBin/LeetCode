116a10d6360ae14776d3d49badbb634d35a0a893
HuskyBin <xiang.zh@husky.neu.edu>
2017-10-03 21:17:31 -0400

    Create Minimum Moves to Equal Array Elements II.java

---
+++ Minimum Moves to Equal Array Elements II.java
@@ -0,0 +1,32 @@
+/*
+Given a non-empty integer array, find the minimum number of moves required to make all array elements equal, where a move is incrementing a selected element by 1 or decrementing a selected element by 1.
+
+You may assume the array's length is at most 10,000.
+
+Example:
+
+Input:
+[1,2,3]
+
+Output:
+2
+
+Explanation:
+Only two moves are needed (remember each move increments or decrements one element):
+
+[1,2,3]  =>  [2,2,3]  =>  [2,2,2]
+*/
+class Solution {
+    public int minMoves2(int[] nums) {
+        if (nums == null) {
+            return 0;
+        }
+        Arrays.sort(nums);
+        int midValue = nums[nums.length / 2];
+        int sum = 0;
+        for (int num : nums) {
+            sum += Math.abs(num - midValue);
+        }
+        return sum;
+    }
+}
