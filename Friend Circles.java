e7f7f74fb20364a5dd70ce516f2bebe59b732abf
HuskyBin <xiang.zh@husky.neu.edu>
2017-10-08 22:02:15 -0400

    Create Friend Circles.java

---
+++ Friend Circles.java
@@ -0,0 +1,95 @@
+/*
+There are N students in a class. Some of them are friends, while some are not. Their friendship is transitive in nature. For example, if A is a direct friend of B, and B is a direct friend of C, then A is an indirect friend of C. And we defined a friend circle is a group of students who are direct or indirect friends.
+
+Given a N*N matrix M representing the friend relationship between students in the class. If M[i][j] = 1, then the ith and jth students are direct friends with each other, otherwise not. And you have to output the total number of friend circles among all the students.
+
+Example 1:
+Input: 
+[[1,1,0],
+ [1,1,0],
+ [0,0,1]]
+Output: 2
+Explanation:The 0th and 1st students are direct friends, so they are in a friend circle. 
+The 2nd student himself is in a friend circle. So return 2.
+Example 2:
+Input: 
+[[1,1,0],
+ [1,1,1],
+ [0,1,1]]
+Output: 1
+Explanation:The 0th and 1st students are direct friends, the 1st and 2nd students are direct friends, 
+so the 0th and 2nd students are indirect friends. All of them are in the same friend circle, so return 1.
+Note:
+N is in range [1,200].
+M[i][i] = 1 for all students.
+If M[i][j] = 1, then M[j][i] = 1.
+
+*/
+// Union Find template
+class Solution {
+    public int findCircleNum(int[][] M) {
+        if (M == null) {
+            return 0;
+        }
+        int n = M.length;
+        UnionFind uf = new UnionFind(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (M[i][j] == 1) {
+                    uf.union(i, j);
+                }
+            }
+        }
+        return uf.count();
+    }
+    
+    class UnionFind {
+        private int[] parent;
+        private int[] size;
+        private int count = 0;
+        
+        public UnionFind(int n) {
+            count = n;
+            parent = new int[n];
+            size = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                size[i] = 1;
+            }
+            
+        }
+        
+        public int find(int p) {
+            while(parent[p] != p) {
+                parent[p] = parent[parent[p]];
+                p = parent[p];
+            }
+            return p;
+        }
+        
+        public void union(int p, int q) {
+            int rootP = find(p);
+            int rootQ = find(q);
+            
+            if (rootP == rootQ) {
+                return;
+            }
+            
+            if (size[rootP] >= size[rootQ]) {
+                parent[rootQ] = rootP;
+                size[rootP] += size[rootQ];
+            }
+            else {
+                parent[rootQ] = rootP;
+                size[rootQ] += size[rootP];
+            }
+            count--;
+        }
+         
+        private int count() {
+            return this.count;
+        }
+        
+        
+    }
+}
