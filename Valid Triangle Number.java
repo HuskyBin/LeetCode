0da33aac76d34e7d35bf8a1293f503dc043704e3
HuskyBin <xiang.zh@husky.neu.edu>
2017-10-13 22:47:14 -0400

    Update Valid Triangle Number.java

--- Valid Triangle Number.java
+++ Valid Triangle Number.java
@@ -13,6 +13,28 @@ Note:
 The length of the given array won't exceed 1000.
 The integers in the given array are in the range of [0, 1000].
 */
+// better solution
+class Solution {
+    public int triangleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int count = 0;
+        for (int i = nums.length - 1; i > 1; i--) {
+            int left = 0;
+            int right = i - 1;
+            while (left < right) {
+                if (nums[left] + nums[right] > nums[i]) {
+                    count += right - left;
+                    right--;
+                }
+                else {
+                    left++;
+                }
+            }
+        }
+        return count;
+    }
+}
+// naive solution
 class Solution {
     public int triangleNumber(int[] nums) {
         Arrays.sort(nums);
