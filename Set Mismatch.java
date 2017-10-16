d364bc4b61f0564aded3dc206838a2ac49ceaab6
HuskyBin <xiang.zh@husky.neu.edu>
2017-10-15 22:44:16 -0400

    Update Set Mismatch.java

--- Set Mismatch.java
+++ Set Mismatch.java
@@ -27,3 +27,31 @@ class Solution {
         return res;
     }
 }
+／／ 归位法
+public class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int[] result = new int[2];
+        
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] - 1 != i && nums[nums[i] - 1] != nums[i]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] - 1 != i) {
+                result[0] = nums[i];
+                result[1] = i + 1;
+                break;
+            }
+        }
+        
+        return result;
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
