1400ee8e7fb3a2c0fa93388987210c63da9e4f8a
HuskyBin <xiang.zh@husky.neu.edu>
2017-10-15 23:08:03 -0400

    Update Maximum Length of Pair Chain.java

--- Maximum Length of Pair Chain.java
+++ Maximum Length of Pair Chain.java
@@ -15,38 +15,18 @@ class Solution {
         if (pairs == null) {
             return 0;
         }
-        Integer[][] boxPair = new Integer[pairs.length][pairs[0].length];
-        for (int i = 0; i < pairs.length; i++) {
-            for (int j = 0; j < pairs[0].length; j++) {
-                boxPair[i][j] = pairs[i][j];
-            }
-        }
-        Arrays.sort(boxPair, new Comparator<Integer[]>() {
-            public int compare(Integer[] a, Integer[] b) {
-                if (a[1] != b[1]) {
-                    return Integer.compare(a[1], b[1]);
-                }
-                else {
-                    return Integer.compare(a[0], b[0]);
-                }
-            }
-        });
-        
+        Arrays.sort(pairs, (a, b) -> (a[0] - b[0]));
         int[] dp = new int[pairs.length];
-        dp[0] = 1;
+        Arrays.fill(dp, 1);
         for (int i = 1; i < pairs.length; i++) {
             int max = 1;
             for (int j = 0; j < i; j++) {
-                if (Integer.compare(boxPair[j][1], boxPair[i][0]) < 0) {
+                if (Integer.compare(pairs[j][1], pairs[i][0]) < 0) {
                     max = Math.max(max, dp[j] + 1);    
                 }
             }
             dp[i] = max;
         }
-        int result = 1;
-        for (int num : dp) {
-            result = Math.max(result, num);
-        }
-        return result;
+        return dp[pairs.length - 1];
     }
 }
