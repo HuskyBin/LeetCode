78681d59d0d540174e82411dabd09e98d1478a1a
HuskyBin <xiang.zh@husky.neu.edu>
2017-10-16 21:34:44 -0400

    Create Palindromic Substrings.java

---
+++ Palindromic Substrings.java
@@ -0,0 +1,40 @@
+/*
+Given a string, your task is to count how many palindromic substrings in this string.
+
+The substrings with different start indexes or end indexes are counted as different substrings even they consist of same characters.
+
+Example 1:
+Input: "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+Example 2:
+Input: "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+Note:
+The input string length won't exceed 1000.
+*/
+class Solution {
+    public int countSubstrings(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            dp[i][i] = true;
+        }
+        int sum = s.length();
+        for (int len = 2; len <= s.length(); len++) {
+            for (int i = 0; i + len - 1 < s.length(); i++) {
+                int j = i + len - 1;
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (len == 2 || dp[i + 1][j - 1]) {
+                        dp[i][j] = true;
+                        sum++;
+                    }
+                }
+            }
+        }
+        return sum;
+    }
+}
