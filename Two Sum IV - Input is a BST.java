2ff36e23922ef7f3c2ae62ca9db0851a727b5dde
HuskyBin <xiang.zh@husky.neu.edu>
2017-10-16 21:48:41 -0400

    Update Two Sum IV - Input is a BST.java

--- Two Sum IV - Input is a BST.java
+++ Two Sum IV - Input is a BST.java
@@ -56,3 +56,40 @@ class Solution {
         return findTarget(pNode.right, set, target);
     }
 }
+// Best solution time O(n), space O(Logn)
+If I have to choose one from them, I would prefer the third one because it takes advantage of the property of BST and is with lower space complexity in average. Its code is shown below.
+
+    public boolean findTarget(TreeNode root, int k) {
+        if(root == null) return false;
+    	Stack<TreeNode> l_stack = new Stack<>();
+    	Stack<TreeNode> r_stack = new Stack<>();
+    	stackAdd(l_stack, root, true);
+    	stackAdd(r_stack, root, false);
+    	while(l_stack.peek() != r_stack.peek()){
+    	    int n = l_stack.peek().val + r_stack.peek().val;
+    	    if(n == k){
+    		return true;
+    	    }else if(n > k){
+    		stackNext(r_stack, false);
+    	    }else{
+		stackNext(l_stack, true);
+    	    }
+    	}
+    	return false;
+    }
+    
+    private void stackAdd(Stack<TreeNode> stack, TreeNode node, boolean isLeft){
+    	while(node != null){
+    	    stack.push(node);
+            node = (isLeft) ? node.left : node.right;
+    	}
+    }
+
+    private void stackNext(Stack<TreeNode> stack, boolean isLeft){
+    	TreeNode node = stack.pop();
+    	if(isLeft){
+    	    stackAdd(stack, node.right, isLeft);
+    	}else{
+    	    stackAdd(stack, node.left, isLeft);
+    	}
+    }
