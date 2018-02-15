89c438dff46ca5af8acf1b4d02e82e82a31a4c4f
HuskyBin <xiang.zh@husky.neu.edu>
2018-02-14 21:23:48 -0500

    Create Toeplitz Matrix.java

---
+++ Toeplitz Matrix.java
@@ -0,0 +1,54 @@
+/*
+A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same element.
+
+Now given an M x N matrix, return True if and only if the matrix is Toeplitz.
+ 
+
+Example 1:
+
+Input: matrix = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
+Output: True
+Explanation:
+1234
+5123
+9512
+
+In the above grid, the diagonals are "[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]", and in each diagonal all elements are the same, so the answer is True.
+Example 2:
+
+Input: matrix = [[1,2],[2,2]]
+Output: False
+Explanation:
+The diagonal "[1, 2]" has different elements.
+Note:
+
+matrix will be a 2D array of integers.
+matrix will have a number of rows and columns in range [1, 20].
+matrix[i][j] will be integers in range [0, 99].
+*/
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int row = 0;
+        int column = matrix[0].length - 1;
+        while (row != matrix.length - 1 || column != 0) {
+            int newRow = row;
+            int newColumn = column;
+            while (newRow >= 0 && newRow < matrix.length && newColumn >= 0 && newColumn < matrix[0].length) {
+                if (matrix[newRow][newColumn] != matrix[row][column]) {
+                    return false;
+                }
+                newRow++;
+                newColumn++;
+            }
+            if (column > 0) {
+                column--;
+            } else {
+                row++;
+            }
+        }
+        return true;
+    }
+}
