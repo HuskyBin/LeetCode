418ed8cfccff386d87e30a11d39e8cbb32278cee
HuskyBin <xiang.zh@husky.neu.edu>
2018-02-25 20:00:20 -0500

    Update Letter Case Permutation.java

--- Letter Case Permutation.java
+++ Letter Case Permutation.java
@@ -16,6 +16,36 @@ S will be a string with length at most 12.
 S will consist only of letters or digits.
 
 */
+// concise solution
+class Solution {
+    public List<String> letterCasePermutation(String S) {
+        List<String> resultList = new ArrayList<>();
+        if (S == null) {
+            return resultList;
+        }
+        dfsCore(S, 0, resultList);
+        return resultList;
+    }
+    
+    private void dfsCore(String str, int index, List<String> resultList) {
+        if (index == str.length()) {
+            resultList.add(str);
+            return;
+        }
+        char curChar = str.charAt(index);
+        if (Character.isLetter(curChar)) {
+            String oneResult = str.substring(0, index) + Character.toUpperCase(curChar) + str.substring(index + 1);
+        
+            dfsCore(oneResult, index + 1, resultList);
+            String secondResult = str.substring(0, index) + Character.toLowerCase(curChar) + str.substring(index + 1);
+            dfsCore(secondResult, index + 1, resultList);    
+        } else {
+            dfsCore(str, index + 1, resultList);
+        }
+    }
+}
+
+// my solution
 class Solution {
     public List<String> letterCasePermutation(String S) {
         List<String> resultList = new ArrayList<>();
