b8a89b982dd7b97999ca5a3a4419cd309cacf219
HuskyBin <xiang.zh@husky.neu.edu>
2018-04-06 18:17:23 -0400

    Update Find Peak Element.java

--- Find Peak Element.java
+++ Find Peak Element.java
@@ -14,6 +14,30 @@ click to show spoilers.
 Credits:
 Special thanks to @ts for adding this problem and creating all test cases.
 */
+
+// Better solution
+class Solution {
+    public int findPeakElement(int[] nums) {
+        if (nums == null) {
+            return -1;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+                return mid;
+            } else if (nums[mid] > nums[mid - 1] && nums[mid] < nums[mid + 1]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return (nums[start] > nums[end]) ? start : end;
+    }
+}
+
+
 public class Solution {
     public int findPeakElement(int[] num) {
         if (num == null || num.length == 0) {
