042ba2cec4de1b054f74253f34ffeb9ff789d343
HuskyBin <xiang.zh@husky.neu.edu>
2018-04-06 18:16:56 -0400

    Update One Edit Distance .java

--- One Edit Distance .java
+++ One Edit Distance .java
@@ -1,6 +1,32 @@
 /*
 Given two strings S and T, determine if they are both one edit distance apart.
 */
+//Better solution
+class Solution {
+    public boolean isOneEditDistance(String s, String t) {
+    	if (s == null || t == null) {
+    		return false;
+    	}
+    	if (Math.abs(s.length() - t.length()) > 1) {
+    		return false;
+    	}
+    	for (int i = 0; i < Math.min(s.length(), t.length()); i++) {
+    		if (s.charAt(i) != t.charAt(i)) {
+    			if (s.length() == t.length()) {
+    				return s.substring(i + 1).equals(t.substring(i + 1));
+    			} else if (s.length() < t.length()) {
+    				return s.substring(i).equals(t.substring(i + 1));
+    			} else {
+    				return s.substring(i + 1).equals(t.substring(i));
+    			}
+    		}
+    	}
+    	return s.length() != t.length();
+    }
+
+}
+
+
 public class Solution {
     public boolean isOneEditDistance(String s, String t) {
         if (Math.abs(s.length() - t.length()) > 1) {
