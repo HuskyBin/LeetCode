bbfc786e61af1506b2298299a7e0a31f9544b81e
HuskyBin <xiang.zh@husky.neu.edu>
2018-06-07 18:11:28 -0400

    Update Insert Delete GetRandom O(1) - Duplicates allowed.java

--- Insert Delete GetRandom O(1) - Duplicates allowed.java
+++ Insert Delete GetRandom O(1) - Duplicates allowed.java
@@ -28,70 +28,56 @@ collection.remove(1);
 // getRandom should return 1 and 2 both equally likely.
 collection.getRandom();
 */
-public class RandomizedCollection {
+class RandomizedCollection {
+    
+    Map<Integer, List<Integer>> map = new HashMap<>();
+    List<Integer> list = new ArrayList<>();
 
-    public List<Integer> list;
-    public int length;
-    public Map<Integer, List<Integer>> map;
 
     /** Initialize your data structure here. */
-
     public RandomizedCollection() {
-        list = new ArrayList<>();
-        length = 0;
-        map = new HashMap<>();
+        
     }
     
     /** Inserts a value to the collection. Returns true if the collection did not already contain the specified element. */
     public boolean insert(int val) {
-        boolean result = true;
-        if (map.containsKey(val) && map.get(val).size() > 0) {
-            result = false;
-            List<Integer> curList = map.get(val);
-            curList.add(length);
-            map.put(val, curList);
-        }
-        else {
-            List<Integer> newList = new ArrayList<>();
-            newList.add(length);
-            map.put(val, newList);
-        }
-        if (length == list.size()) {
-            list.add(val);
-        }
-        else {
-            list.set(length, val);
-        }
-        length++;
+        boolean result = map.containsKey(val) ? false : true;
+        map.computeIfAbsent(val, k -> new ArrayList<>()).add(list.size());
+        list.add(val);
         return result;
     }
     
     /** Removes a value from the collection. Returns true if the collection contained the specified element. */
     public boolean remove(int val) {
-        if (!map.containsKey(val) || (map.get(val).size() == 0)) {
+        if (!map.containsKey(val)) {
             return false;
         }
-        List<Integer> curList = map.get(val);
-        int index = curList.remove(curList.size() - 1);
-        map.put(val, curList);
-        int lastElement = list.get(length - 1);
-        list.set(index, lastElement);
-        List<Integer> lastEleIndex = map.get(lastElement);
-        for(int i = 0; i < lastEleIndex.size(); i++) {
-            if (lastEleIndex.get(i) == (length - 1)) {
-                lastEleIndex.remove(i);
-                lastEleIndex.add(index);
+        List<Integer> indexList = map.get(val);
+        int removeIndex = indexList.get(indexList.size() - 1);
+        indexList.remove(indexList.size() - 1);
+        int lastVal = list.get(list.size() - 1);
+ 
+        list.set(removeIndex, lastVal);
+            
+        List<Integer> lastValIndex = map.get(lastVal);
+        for (int i = 0; i < lastValIndex.size(); i++) {
+            if (lastValIndex.get(i) == (list.size() - 1)) {
+                lastValIndex.remove(i);
+                lastValIndex.add(removeIndex);
                 break;
             }
         }
-        length--;
+        if (indexList.size() == 0) {
+            map.remove(val);
+        }
+        list.remove(list.size() - 1);
         return true;
     }
     
     /** Get a random element from the collection. */
     public int getRandom() {
         Random random = new Random();
-        int nextInt = random.nextInt(length);
-        return list.get(nextInt);
+        int index = random.nextInt(list.size());
+        return list.get(index);
     }
 }
