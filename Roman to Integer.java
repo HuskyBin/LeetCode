b753a5d9a762c7ef950be2f615bfb967cb310b50
HuskyBin <xiang.zh@husky.neu.edu>
2018-06-11 23:04:17 -0400

    Update Roman to Integer.java

--- Roman to Integer.java
+++ Roman to Integer.java
@@ -3,6 +3,35 @@ Given a roman numeral, convert it to an integer.
 
 Input is guaranteed to be within the range from 1 to 3999.
 */
+
+// better
+class Solution {
+    public int romanToInt(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (i == 0 || (map.get(c) <= map.get(s.charAt(i - 1)))) {
+                result += map.get(c);
+            } else {
+                result += map.get(c) - 2 * map.get(s.charAt(i - 1));
+            }
+        }
+        return result;
+    }
+}
+
+
 public class Solution {
     public int romanToInt(String s) {
         if (s == null || s.length() == 0) {
