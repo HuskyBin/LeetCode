58f636cd1d41e900beec0f3da1f661126f3f9c8b
HuskyBin <xiang.zh@husky.neu.edu>
2018-08-08 23:19:17 -0400

    Update Trapping Rain Water.java

--- Trapping Rain Water.java
+++ Trapping Rain Water.java
@@ -8,6 +8,38 @@ Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
 The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped. Thanks Marcos for contributing this image!
 */
 // Be carefull this case [2,0,2], that means you need caculate just once for this equal case.
+// Best Solution
+class Solution {
+    public int trap(int[] height) {
+        if (height == null) {
+            return 0;
+        }
+        int result = 0;
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (leftMax < height[left]) {
+                    leftMax = height[left];
+                } else {
+                    result += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (rightMax < height[right]) {
+                    rightMax = height[right];
+                } else {
+                    result += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+        return result;
+    }
+}
+
 public class Solution {
     public int trap(int[] A) {
         if (A == null || A.length == 0) {
