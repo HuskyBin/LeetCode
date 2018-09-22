71efdc043ccf8132855c51a3c2942220da7a7bc9
HuskyBin <xiang.zh@husky.neu.edu>
2018-09-22 14:46:03 -0400

    Create Fair Candy Swap.java

---
+++ Fair Candy Swap.java
@@ -0,0 +1,64 @@
+/*
+Alice and Bob have candy bars of different sizes: A[i] is the size of the i-th bar of candy that Alice has, and B[j] is the size of the j-th bar of candy that Bob has.
+
+Since they are friends, they would like to exchange one candy bar each so that after the exchange, they both have the same total amount of candy.  (The total amount of candy a person has is the sum of the sizes of candy bars they have.)
+
+Return an integer array ans where ans[0] is the size of the candy bar that Alice must exchange, and ans[1] is the size of the candy bar that Bob must exchange.
+
+If there are multiple answers, you may return any one of them.  It is guaranteed an answer exists.
+
+ 
+
+Example 1:
+
+Input: A = [1,1], B = [2,2]
+Output: [1,2]
+Example 2:
+
+Input: A = [1,2], B = [2,3]
+Output: [1,2]
+Example 3:
+
+Input: A = [2], B = [1,3]
+Output: [2,3]
+Example 4:
+
+Input: A = [1,2,5], B = [2,4]
+Output: [5,4]
+ 
+
+Note:
+
+1 <= A.length <= 10000
+1 <= B.length <= 10000
+1 <= A[i] <= 100000
+1 <= B[i] <= 100000
+It is guaranteed that Alice and Bob have different total amounts of candy.
+It is guaranteed there exists an answer.
+*/
+class Solution {
+    public int[] fairCandySwap(int[] A, int[] B) {
+        if (A == null || B == null) {
+            return new int[0];
+        }
+        Set<Integer> set = new HashSet<>();
+        int aSum = 0;
+        int bSum = 0;
+        for (int a : A) {
+            aSum += a;
+        }
+        for (int b : B) {
+            bSum += b;
+        }
+        int diff = (aSum - bSum) / 2;
+        for (int num : A) {
+            set.add(num);
+        }
+        for (int num : B) {
+            if (set.contains(num + diff)) {
+                return new int[]{num + diff, num};
+            }
+        }
+        return new int[0];
+    }
+}
